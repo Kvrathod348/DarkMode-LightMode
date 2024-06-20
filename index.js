63edc1969594725ea2211d3a45501a8f5c9ba4b8
Kvrathod348 <kvrathod348@gmail.com>
2024-06-20 16:48:32 +0530

    function added

---
+++ index.js
@@ -0,0 +1,22 @@
+function light(){
+    const lightMode=document.getElementById('light')
+    lightMode.classList.add('active');
+
+    const darkMood=document.getElementById('dark');
+    darkMood.classList.remove('active');
+
+    const bodyMood=document.getElementById('body');
+    bodyMood.classList = "bg-light"
+
+}
+
+function dark(){
+    const darkMood=document.getElementById('dark');
+    darkMood.classList.add('active');
+
+    const lightMode=document.getElementById('light')
+    lightMode.classList.remove('active');
+
+    const bodyMood=document.getElementById('body');
+    bodyMood.classList= "bg-dark"
+}
